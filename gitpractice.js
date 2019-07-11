363fe807a29d09555842fccfdf82ffe243f922bd
kevinfASC6 <kevinfbths@gmail.com>
2019-07-11 14:49:26 -0400

    first commit

---
+++ gitpractice.js
@@ -0,0 +1,17 @@
+// function that multiplies all values in array by 2 and prints the new array
+// someArray = [10, 20, 30, 40] 
+//10 20 30 40 
+function multiplyArrayNumbers(arr) { 
+  //create a loop 
+  //thatwill go through each individual element of the array
+  //and multiply the value by 2 
+
+  for(i = 0; i < arr.length; i++){ 
+      //that will go through each individual element 
+      // of the array 
+      //and multiply the value by 2  
+     arr[i] =  arr[i]*2 //arr[i]*=2
+  }
+
+
+}
